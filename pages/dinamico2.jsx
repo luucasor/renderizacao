e0eacb4c99f105dc921f9484496bbb1369352830
luucasor <lucas@jornadatecnologia.com>
2023-06-23 15:28:55 -0300

    Exemplo comparando getStaticProps x getServerSideProps

--- pages/dinamico2.jsx
+++ pages/dinamico2.jsx
@@ -1,4 +1,4 @@
-export async function getStaticProps() {
+export async function getServerSideProps() {
     console.log('[Server] gerando props para o componente...')
     const resp = await fetch('http://localhost:3000/api/produtos')
     const produtos = await resp.json()
@@ -10,7 +10,7 @@ export async function getStaticProps() {
     }
 }
 
-export default function Estatico4(props) {
+export default function Dinamico2(props) {
     console.log('[Cliente] renderizando o componente...')
     function renderizarProdutos() {
         return props.produtos.map(produto => {
@@ -23,7 +23,7 @@ export default function Estatico4(props) {
     }
     return (
         <div>
-            <h1>Estático #4</h1>
+            <h1>Dinâmico #2</h1>
             <ul>
                 {renderizarProdutos()}
             </ul>
