c4abe47c2c520adcfb7e06a2666de3ea99dc86b2
luucasor <lucas@jornadatecnologia.com>
2023-06-23 16:02:27 -0300

    Exemplo com o uso de getStaticPaths e fallback: true. Nesse caso se o id não for encontrado no conteúdo estático, a função getStaticProps é acionada para buscar o conteúdo do id faltante. E nas próximas requisições este cara já estará disponível, sem a necessidade de busca pelo getStaticProps. Resumindo, o fallback habilita ou não o uso da função getStaticProps

--- pages/alunos/[id].jsx
+++ pages/alunos/[id].jsx
@@ -7,12 +7,13 @@ export async function getStaticPaths() {
     })
 
     return {
-        fallback: false, //404
+        fallback: true, //404
         paths
     }
 }
 
 export async function getStaticProps({ params }) {
+    console.log(params)
     const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
     const aluno = await resp.json()
     return {
@@ -27,11 +28,14 @@ export default function AlunoPorId(props) {
     return (
         <div>
             <h1>Detalhes do Aluno</h1>
-            <ul>
-                <li>{aluno.id}</li>
-                <li>{aluno.nome}</li>
-                <li>{aluno.email}</li>
-            </ul>
+            {aluno ?
+                <ul>
+                    <li>{aluno.id}</li>
+                    <li>{aluno.nome}</li>
+                    <li>{aluno.email}</li>
+                </ul>
+            : false
+            }
         </div>
     )
 }
