9b13846dbe76e440b8dbebe6c81444db71d3500f
luucasor <lucas@jornadatecnologia.com>
2023-06-23 14:56:27 -0300

    Exemplo com uso de api e listagem de produtos

---
+++ pages/estatico4.jsx
@@ -0,0 +1,26 @@
+export async function getStaticProps() {
+    const resp = await fetch('http://localhost:3000/api/produtos')
+    const produtos = await resp.json()
+
+    return {
+        props: {
+            produtos
+        }
+    }
+}
+
+export default function Estatico4(props) {
+    function renderizarProdutos() {
+        return props.produtos.map(produto => {
+            return <li key={produto.id}>{produto.nome} tem preço de R${produto.preco}</li>
+        })
+    }
+    return (
+        <div>
+            <h1>Estático #4</h1>
+            <ul>
+                {renderizarProdutos()}
+            </ul>
+        </div>
+    )
+}
